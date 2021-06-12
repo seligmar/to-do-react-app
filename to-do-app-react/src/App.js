317ebd8466e873634ab76f27a67ec01ab83eef67
github username <github email address>
2021-06-12 11:38:33 +0100

    added some very lazy styling and a bumblebee image

--- to-do-app-react/src/App.js
+++ to-do-app-react/src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
 
 function App () {
-  return <div className='App' />
+  return (
+    <div className='App'>
+      <h1>
+        <img
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXefUCqu9sYksIPalsQiYkx7SpfGyFCgx4aCQ3_d0JjE-0aZv7WRBdsvYZ5RAWYmM3O4Km64oMsAo&usqp=CAc'
+          alt='bumblebee'
+        />
+        My To-Do List
+        <img
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXefUCqu9sYksIPalsQiYkx7SpfGyFCgx4aCQ3_d0JjE-0aZv7WRBdsvYZ5RAWYmM3O4Km64oMsAo&usqp=CAc'
+          alt='bumblebee'
+        />
+      </h1>
+    </div>
+  )
 }
 
 export default App
