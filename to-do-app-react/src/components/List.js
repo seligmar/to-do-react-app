978a9bb8a88aa6ca54889226912e2beb191f25bd
github username <github email address>
2021-07-03 15:58:00 +0100

    can now add tasks to list

--- to-do-app-react/src/components/List.js
+++ to-do-app-react/src/components/List.js
@@ -1,12 +1,13 @@
-// const List = props => {
-//   const tasks = props.tasks
-//   return (
-//     <div>
-//       {tasks.array.forEach(task => {
-//         ;<li key={task.id}>{task.name}</li>
-//       })}
-//     </div>
-//   )
-// }
+const List = props => {
+  console.log(props.tasks)
+  // const tasks = props.tasks
+  return (
+    <ul>
+      {props.tasks.map(task => (
+        <li key={task.id}>{task.taskName}</li>
+      ))}
+    </ul>
+  )
+}
 
-// export default List
+export default List
